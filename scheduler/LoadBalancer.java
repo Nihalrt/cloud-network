2b2ac9bae1487dfb1236683646965974663d5b54
Sai Nihal Diddi <sainihaldiddi@uvic.ca>
2025-02-26 23:56:13 -0800

    created-LeastVM-Scheduler

--- scheduler/LoadBalancer.java
+++ scheduler/LoadBalancer.java
@@ -1,4 +1,21 @@
 package scheduler;
 
+import core.VirtualMachine;
+import java.util.*;
+
 public class LoadBalancer {
+
+    public static VirtualMachine getLeastLoadedVM(List<VirtualMachine> vms) {
+        if (vms == null || vms.size() == 0) {
+            return null;
+        }
+        VirtualMachine leastvm = vms.get(0);
+        for (VirtualMachine vm : vms) {
+            if(vm.getLoad() < leastvm.getLoad()){
+                leastvm = vm;
+            }
+        }
+        return leastvm;
+    }
+
 }
--- scheduler/Scheduler.java
+++ scheduler/Scheduler.java
@@ -1,4 +1,31 @@
 package scheduler;
 
+import core.*;
+import java.util.*;
+
 public class Scheduler {
+    private PriorityQueue<Task> taskQueue;
+
+    public Scheduler() {
+        taskQueue = new PriorityQueue<>();
+
+    }
+    public void addTask(Task task) {
+        taskQueue.offer(task);
+        System.out.println("Scheduler added task: " + task);
+
+    }
+    public void scheduleTasks(List<VirtualMachine> vms){
+        while (!taskQueue.isEmpty()){
+            Task task = taskQueue.poll();
+            VirtualMachine selectedVM = LoadBalancer.getLeastLoadedVM(vms);
+            if (selectedVM != null){
+                selectedVM.addTask(task);
+            }
+            else{
+                System.out.println("No VM available for task: " + task.getTaskId());
+            }
+        }
+    }
+
 }
